1e8eab4cfe86aac233797ab6fbb3bb5015582fc9
Akizon <hytf233g@gmail.com>
2023-11-02 12:46:20 +0800

    添加项目文件。

---
+++ MVVM/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MotdEditor.MVVM;
+
+namespace MotdEditor
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow
+    {
+        MainWindowViewModel ViewModel { get; }
+
+        public MainWindow(MainWindowViewModel viewModel, IServiceProvider serviceProvider)
+        {
+            ViewModel = viewModel;
+            DataContext = this;
+            InitializeComponent();
+        }
+    }
+}
---
+++ Services/ApplicationHostService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotdEditor.Services
+{
+    internal class ApplicationHostService : IHostedService
+    {
+        public readonly IServiceProvider _serviceProvider;
+        public ApplicationHostService(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            await Task.CompletedTask;
+            var mainwindow = _serviceProvider.GetRequiredService<MainWindow>();
+            mainwindow.Show();
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
